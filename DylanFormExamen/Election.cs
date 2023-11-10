9b7b8017108b05397bb4cbd21bffb82b5d8800f5
info1 <1852719@cegepoutaouais.qc.ca>
2023-11-10 11:30:12 -0500

    commitAjoutez des fichiers projet.

---
+++ DylanFormExamen/Election.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DylanFormExamen
+{
+    public static class Election
+    {
+        //Attributs privée
+        private static string nom_election;
+        private static DateTime date_election;
+        private static List<Electeur> list_electeurs;
+
+        //propriéter
+        public static string NomElection
+        {
+            get { return nom_election; }
+            set { nom_election = value; }
+        }
+
+        public static DateTime DateElection
+        {
+            get { return date_election; }
+            set { date_election = value; }
+        }
+
+        public static List<Electeur> listElecteurs
+        {
+            get { return list_electeurs; }
+            set { list_electeurs = value; }
+        }
+
+
+        //constructeur static
+        static Election()
+        {
+            listElecteurs = new List<Electeur>();
+        }
+
+        //methode ajouter electeur
+        public static string ajouter_electeur(Electeur p_elect)
+        {
+            //p_elect.(listElecteurs);
+        }
+
+        public static string rechercher_electeurs_ciconscription(string ciconscription)
+        {
+            int count = 0;
+            foreach (Electeur e in listElecteurs)
+                if (e.Circoncription == ciconscription) count++;
+            return ciconscription;
+        }
+
+        public static
+    }
+}
